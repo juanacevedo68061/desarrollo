a0dd7a3dd90082ccf92ae642d9de643402b5e331
juanacevedo68061 <111782315+juanacevedo68061@users.noreply.github.com>
2022-09-28 12:49:05 -0400

    Update settings.py
    Cambiando a Sqlite3 como base de datos para desarrollo

--- General/settings.py
+++ General/settings.py
@@ -100,15 +100,10 @@ SOCIALACCOUNT_PROVIDERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'base2',
-        'USER': 'postgres',
-        'PASSWORD':'aezakmi',
-        'HOST':'localhost',
-        'PORT':'5433',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
